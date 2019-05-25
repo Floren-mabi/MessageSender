a61321284c23351cec8d38711c32075b10ad1fbb
Floren_mabi <51026455+Floren-mabi@users.noreply.github.com>
2019-05-26 00:50:59 +0900

    Initial

---
+++ MessageSender/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length == 0) { return; }
+            string message = args[0];
+            using (var stream = new NamedPipeClientStream("SEServer"))
+            {
+                stream.Connect(10000);
+                using (var writer = new BinaryWriter(stream, Encoding.UTF8))
+                {
+                    writer.Write(message);
+                }
+            }
+        }
+    }
+}
